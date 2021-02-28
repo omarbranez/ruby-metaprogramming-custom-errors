22a7989148bbe89cfa8464ad2ec2bba5b23f36ef
Omar Branez <omar.branez@gmail.com>
2021-02-27 20:09:29 -0500

    commit tree 190440588d890953fd3c3e9cd5c5d9afe18f086e

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,8 +6,24 @@ class Person
   end
 
   def get_married(person)
+    # self.partner = person
+    # person.partner = self
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin #beginning of exception
+        raise PartnerError # raises PartnerError if the condition is true (when get_married is not an instance of person)
+      rescue PartnerError => error #_rescue_ begins instance of PartnerError called error
+          puts error.message #runs message in PartnerError
+      end #begin to end
+    else
+      person.partner = self
+    end
+  end
+
+  class PartnerError < StandardError #PartnerError inherits from StandardError, which contains NameError, which contains _NoMethodError_
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
 
 end
@@ -15,7 +31,3 @@ end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
